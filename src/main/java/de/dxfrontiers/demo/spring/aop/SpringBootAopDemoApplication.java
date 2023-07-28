6eef878079eaee393beee7539a52ac6304808d88
Nik101010 <git@nik101010.de>
2023-07-28 10:25:21 +0200

    initial spring boot project created by start.spring.io

---
+++ src/main/java/de/dxfrontiers/demo/spring/aop/SpringBootAopDemoApplication.java
@@ -0,0 +1,13 @@
+package de.dxfrontiers.demo.spring.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAopDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAopDemoApplication.class, args);
+	}
+
+}
