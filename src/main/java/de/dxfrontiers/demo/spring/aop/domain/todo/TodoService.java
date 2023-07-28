39849b390ad795139f2aad00f9b08462181c819b
Nik101010 <git@nik101010.de>
2023-07-28 11:23:04 +0200

    add logging for more stable operation

--- src/main/java/de/dxfrontiers/demo/spring/aop/domain/todo/TodoService.java
+++ src/main/java/de/dxfrontiers/demo/spring/aop/domain/todo/TodoService.java
@@ -1,8 +1,10 @@
 package de.dxfrontiers.demo.spring.aop.domain.todo;
 
+import org.slf4j.Logger;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
+import java.util.List;
 
 @Service
 public class TodoService {
@@ -14,19 +16,28 @@ public class TodoService {
     }
 
     public Collection<TodoEntity> listTodos() {
-        return todoRepository.findAll();
+        LOG.trace("entering method listTodos");
+        List<TodoEntity> todos = todoRepository.findAll();
+        LOG.trace("leaving method listTodos, returning: " + todos);
+        return todos;
     }
 
     public TodoEntity createTodo(String todo) {
+        LOG.trace("entering method createTodo");
         TodoEntity todoEntity = new TodoEntity();
         todoEntity.setTodo(todo);
         todoRepository.save(todoEntity);
+        LOG.trace("leaving method createTodo, returning: " + todoEntity);
         return todoEntity;
     }
 
     public void markAsDone(long todoId) {
+        LOG.trace("entering method markAsDone");
         TodoEntity todoEntity = todoRepository.findById(todoId).orElseThrow(() -> new RuntimeException("Todo not found: " + todoId));
         todoEntity.setDone(true);
         todoRepository.save(todoEntity);
+        LOG.trace("leaving method markAsDone");
     }
+
+    private static final Logger LOG = org.slf4j.LoggerFactory.getLogger(TodoService.class);
 }
