abce77d21682e01c3f28a670584130eb46a52ac5
Michael Graf <michael.graf@digitalfrontiers.de>
2023-08-01 04:25:53 +0200

    Extended logger with exectution time.

--- src/main/java/de/dxfrontiers/demo/spring/aop/aspects/logging/LoggerAspect.java
+++ src/main/java/de/dxfrontiers/demo/spring/aop/aspects/logging/LoggerAspect.java
@@ -1,9 +1,11 @@
 package de.dxfrontiers.demo.spring.aop.aspects.logging;
 
-import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
 
 
 @Aspect
@@ -29,36 +31,38 @@ public class LoggerAspect {
     private void pointcut() {}
 
     /**
-     * Advice that is executed before the annotated method call (see pointcut).
-     * @param joinPoint object that gives the advice the runtime information of the join point
+     * Around advice that is executed around the defined pointcut.
+     * @param pjp the ProceedingJoinPoint object. .proceed() has to be called by us (if we want).
+     * @return returns the return value. We can modify it at runtime.
+     * @throws Throwable we can throw an own exception, but it should match the underling method signature.
      */
-    @Before("pointcut()")
-    public void before(JoinPoint joinPoint) {
-        LoggerFactory.getLogger(joinPoint.getSignature().getDeclaringType()).trace("Calling {} with {}", joinPoint.getSignature().getName(), joinPoint.getArgs());
-    }
-
-    /**
-     * Advice that is executed before the annotated method call (see pointcut).
-     * @param joinPoint object that gives the advice the runtime information of the join point
-     * @param retVal object that was returned by the target method
-     */
-    @AfterReturning(value = "pointcut()", returning = "retVal")
-    public void afterReturn(JoinPoint joinPoint, Object retVal) {
-        LoggerFactory.getLogger(joinPoint.getSignature().getDeclaringType()).trace("{} called with {} returned {}", joinPoint.getSignature().getName(), joinPoint.getArgs(), retVal);
-    }
-
-    /**
-     * Advice that is executed before the annotated method call (see pointcut).
-     * @param jp object that gives the advice the runtime information of the join point
-     * @param ex exception that was by the target method
-     */
-    @AfterThrowing(value = "pointcut()", throwing = "ex")
-    public void afterThrowing(JoinPoint jp, Throwable ex) {
-        LoggerFactory.getLogger(jp.getSignature().getDeclaringType()).trace(
-                "Method {} called with {} threw {}",
-                jp.getSignature().getName(),
-                jp.getArgs(),
-                ex.toString()
-        );
+    @Around("pointcut()")
+    public Object around(ProceedingJoinPoint pjp) throws Throwable {
+        Logger logger = LoggerFactory.getLogger(pjp.getSignature().getDeclaringType());
+        logger.trace("Calling {} with {}", pjp.getSignature().getName(), pjp.getArgs());
+        StopWatch sw = new StopWatch(pjp.toLongString());
+        sw.start();
+        try {
+            Object retVal = pjp.proceed();
+            sw.stop();
+            logger.trace(
+                    "Method {} called with {} returned {} executed in {} ms",
+                    pjp.getSignature().getName(),
+                    pjp.getArgs(),
+                    retVal,
+                    sw.getTotalTimeMillis()
+            );
+            return retVal;
+        } catch (Throwable ex) {
+            sw.stop();
+            logger.trace(
+                    "Method {} called with {} threw {} after {} ms",
+                    pjp.getSignature().getName(),
+                    pjp.getArgs(),
+                    ex,
+                    sw.getTotalTimeMillis()
+            );
+            throw ex;
+        }
     }
 }
